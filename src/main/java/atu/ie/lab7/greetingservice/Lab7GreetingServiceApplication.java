e3820e11d0a03c53b573919ad5f8dd9e33d5db6c
ERNESTOALONSOPEREZ-S <g00436110@atu.ie>
2025-12-05 09:39:28 +0000

    added files

---
+++ src/main/java/atu/ie/lab7/greetingservice/Lab7GreetingServiceApplication.java
@@ -0,0 +1,13 @@
+package atu.ie.lab7.greetingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7GreetingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab7GreetingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/lab7/greetingservice/Lab7GreetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.lab7.greetingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7GreetingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
